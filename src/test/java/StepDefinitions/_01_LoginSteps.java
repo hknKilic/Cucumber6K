7a74d5ce58eaf1778f3847553714f358fb297b56
hknKilic <Aliyamnuveybam@gmail.com>
2023-09-15 17:15:54 +0200

    Initial commit

---
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -0,0 +1,39 @@
+package StepDefinitions;
+
+import Pages.DialogContent;
+import Utilities.GWD;
+import io.cucumber.java.en.*;
+
+public class _01_LoginSteps {
+    DialogContent dc=new DialogContent();
+
+    @Given("Navigate to Campus")
+    public void navigate_to_campus() {
+        //System.out.println("site açıldı");
+        GWD.getDriver().get("https://test.mersys.io/");
+
+    }
+
+    @When("Enter username and password and click login button")
+    public void enter_username_and_password_and_click_login_button() {
+        //System.out.println("username ve password girildi");
+
+//        WebDriverWait wait=new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(20));
+//        wait.until(ExpectedConditions.visibilityOf(dc.username));
+//
+//        dc.username.sendKeys("turkeyts");
+//        dc.password.sendKeys("TechnoStudy123");
+        // dc.loginButton.click();
+
+        dc.sendKeys(dc.username,"turkeyts");
+        dc.sendKeys(dc.password,"TechnoStudy123");
+        dc.click(dc.loginButton);
+    }
+
+    @Then("User should login successfully")
+    public void user_should_login_successfully() {
+        System.out.println("girildiği test edildi");
+    }
+
+}
+
